6325705f4b1e204fa763899be8addc99d2e27d99
koku <koku+gc@koku.dev>
2021-09-04 22:21:58 +0900

    Remove unused import

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,13 @@
 #[cfg(feature = "float")]
 extern crate num_traits;
 
-#[allow(unused_imports)]
-use assertions::basic::{ComparableAssertion, EqualityAssertion};
-#[allow(unused_imports)]
-use assertions::float::FloatAssertion;
-#[allow(unused_imports)]
-use assertions::iterator::IteratorAssertion;
-#[allow(unused_imports)]
-use assertions::map::MapAssertion;
-#[allow(unused_imports)]
-use assertions::result::ResultAssertion;
-#[allow(unused_imports)]
-use assertions::testing::AssertionResultAssertion;
-#[allow(unused_imports)]
-use base::{AssertionResult, CheckThatResult, Fact, Location, ReturnStrategy, Subject};
-
 mod assertions;
 mod base;
+
+pub use assertions::basic::{ComparableAssertion, EqualityAssertion};
+pub use assertions::float::FloatAssertion;
+pub use assertions::iterator::IteratorAssertion;
+pub use assertions::map::MapAssertion;
+pub use assertions::result::ResultAssertion;
+pub use assertions::testing::AssertionResultAssertion;
+pub use base::{AssertionResult, CheckThatResult, Fact, Location, ReturnStrategy, Subject};
