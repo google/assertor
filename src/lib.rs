660ca76ea953f4579d8a63707e2b5045dfacbcb5
koku <koku+gc@koku.dev>
2021-09-05 07:39:45 +0900

    Write example in root library doc

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,32 @@
 //!
 //! Assertor is heavy affected by [Java Truth](https://github.com/google/truth) in terms of API
 //! design and error messages.
+//!
+//! # Example
+//! ```
+//! use assertor::*;
+//!
+//! assert_that!("foobarbaz").contains("bar");
+//! assert_that!("foobarbaz").ends_with("baz");
+//!
+//! assert_that!(0.5).with_abs_tol(0.2).is_approx_equal_to(0.6);
+//!
+//! assert_that!(vec!["a", "b"]).contains("a");
+//! assert_that!(vec!["a", "b"]).has_length(2);
+//! assert_that!(vec!["a", "b"]).contains_exactly(vec!["a", "b"]);
+//!
+//! assert_that!(Option::Some("Foo")).has_value("Foo");
+//! ```
+//! ## Failure cases
+//! ```should_panic
+//! use assertor::*;
+//! assert_that!(vec!["a", "b", "c"]).contains_exactly(vec!["b", "c", "d"]);
+//! // missing (1)   : ["d"]
+//! // unexpected (1): ["a"]
+//! // ---
+//! // expected      : ["b", "c", "d"]
+//! // actual        : ["a", "b", "c"]
+//! ```
 #![warn(missing_docs)]
 
 #[cfg(feature = "float")]
@@ -12,6 +38,7 @@ pub use assertions::basic::{ComparableAssertion, EqualityAssertion};
 pub use assertions::float::FloatAssertion;
 pub use assertions::iterator::IteratorAssertion;
 pub use assertions::map::MapAssertion;
+pub use assertions::option::OptionAssertion;
 pub use assertions::result::ResultAssertion;
 pub use assertions::set::SetAssertion;
 pub use assertions::string::StringAssertion;
